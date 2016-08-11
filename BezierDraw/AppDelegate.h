c45128a54eedc4df6db6e791b425b37964e3c4c7
andyhaz <santelife@gmail.com>
2016-08-10 19:34:13 -0700

    Initial Commit

---
+++ BezierDraw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BezierDraw
+//
+//  Created by andrew hazlett on 8/10/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BezierDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BezierDraw
+//
+//  Created by andrew hazlett on 8/10/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
